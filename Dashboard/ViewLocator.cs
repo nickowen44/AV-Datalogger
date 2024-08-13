ba341d12c51cd45ab1b1883df473f47a02dc74e7
nrdag <25031285+nrdag@users.noreply.github.com>
2024-08-13 12:13:35 +1000

    Applied editorformat to all files

--- Dashboard/ViewLocator.cs
+++ Dashboard/ViewLocator.cs
@@ -7,12 +7,11 @@ namespace Dashboard;
 
 public class ViewLocator : IDataTemplate
 {
-
     public Control? Build(object? data)
     {
         if (data is null)
             return null;
-        
+
         var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         var type = Type.GetType(name);
 
@@ -22,7 +21,7 @@ public class ViewLocator : IDataTemplate
             control.DataContext = data;
             return control;
         }
-        
+
         return new TextBlock { Text = "Not Found: " + name };
     }
 
