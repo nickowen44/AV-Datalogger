e83a04a6e28c425068d22f03b57dbef35a889389
AriadneVentura <s3902040@student.rmit.edu.au>
2024-09-16 14:53:50 +1000

    ADL-33 Ignore to old tests

--- DashboardTests/DemoTest.cs
+++ DashboardTests/DemoTest.cs
@@ -20,6 +20,80 @@ public class DemoTest
 
     private Mock<IDataStore> _dataStore;
 
+
+    [AvaloniaTest]
+    [Ignore("Waiting on ADL-32 PR as currently the main window is scurtineering view")]
+    public void TestMainWindow()
+    {
+        // Arrange
+        var window = new MainWindow
+        {
+            DataContext = new MainViewModel(_dataStore.Object)
+        };
+
+        // Act
+        window.Show();
+
+        // Assert
+        Assert.That(window, Is.Not.Null);
+        Assert.That(window, Is.InstanceOf<MainWindow>());
+
+        var speed = window.FindControl<TextBlock>("SpeedDisplay");
+        var steeringAngle = window.FindControl<TextBlock>("SteeringAngleDisplay");
+        var brakePressure = window.FindControl<TextBlock>("BrakePressureDisplay");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(speed, Is.Not.Null);
+            Assert.That(steeringAngle, Is.Not.Null);
+            Assert.That(brakePressure, Is.Not.Null);
+        });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(speed.Text, Is.EqualTo("Speed: 0"));
+            Assert.That(steeringAngle.Text, Is.EqualTo("Steering Angle: 0"));
+            Assert.That(brakePressure.Text, Is.EqualTo("Brake Pressure: 0"));
+        });
+    }
+
+    [AvaloniaTest]
+    [Ignore("Waiting on ADL-32 PR as currently the main window is scurtineering view")]
+    public void TestDataStore()
+    {
+        // Arrange
+        _dataStore.SetupGet(x => x.Speed).Returns(50);
+        _dataStore.SetupGet(x => x.SteeringAngle).Returns(90);
+        _dataStore.SetupGet(x => x.BrakePressure).Returns(100);
+
+        var window = new MainWindow
+        {
+            DataContext = new MainViewModel(_dataStore.Object)
+        };
+
+        // Act
+        window.Show();
+
+        // Assert
+        var speed = window.FindControl<TextBlock>("SpeedDisplay");
+        var steeringAngle = window.FindControl<TextBlock>("SteeringAngleDisplay");
+        var brakePressure = window.FindControl<TextBlock>("BrakePressureDisplay");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(speed, Is.Not.Null);
+            Assert.That(steeringAngle, Is.Not.Null);
+            Assert.That(brakePressure, Is.Not.Null);
+        });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(speed.Text, Is.EqualTo("Speed: 50"));
+            Assert.That(steeringAngle.Text, Is.EqualTo("Steering Angle: 90"));
+            Assert.That(brakePressure.Text, Is.EqualTo("Brake Pressure: 100"));
+        });
+    }
+
     [AvaloniaTest]
     public void TestScrutineeringViewCorrectlyPopulatesCarouselWithYamlData()
     {
@@ -108,6 +182,4 @@ public class DemoTest
             Dispatcher.UIThread.RunJobs();
         }
     }
-
-    // TODO IDK how to test bad yaml cause you send in the data context which has the yaml data that would have been already adjusted by the view model, so it is not replicatable (i think) by tests
 }
