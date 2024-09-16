3a6c5e23e22638e34ab698a686d24916665dfbe8
Richard Quach <82982007+S3784866@users.noreply.github.com>
2024-09-16 21:46:44 +1000

    Fixed last broken test

--- DashboardTests/ScrutineeringTests.cs
+++ DashboardTests/ScrutineeringTests.cs
@@ -25,7 +25,7 @@ public class ScrutineeringTests
     public void TestScrutineeringViewCorrectlyPopulatesCarouselWithYamlData()
     {
         // Arrange
-        var window = new ScrutineeringView()
+        var window = new ScrutineeringView
         {
             DataContext = new ScrutineeringViewModel(_dataStore.Object)
         };
@@ -63,44 +63,52 @@ public class ScrutineeringTests
     public void TestScrutineeringViewCorrectlyDisplaysDvDataIfMeasurmentsIsPresent()
     {
         // Arrange
-        var window = new ScrutineeringView
+        // Create a window with the ScrutineeringView as its content for rendering purposes.
+        var window = new Window
         {
+            Content   = new ScrutineeringView(),
             DataContext = new ScrutineeringViewModel(_dataStore.Object)
         };
         
-        var carousel = window.FindControl<Carousel>("Slides");
-     
-        // We need to loop through each Item to get each container for the slide to find the textblock.
-        for (var i = 0; i < carousel.ItemCount; i++)
+        window.Show();
+        // Get the Carousel
+        // Have to do it in this odd fashion as Avalonia does not trigger the full rendering process with a UserControl View
+        // and the Dv data was not being properly rendered so it was not being picked up.
+        foreach (var carousel in window.GetVisualDescendants().OfType<Carousel>())
         {
-            var container = carousel.ItemContainerGenerator.ContainerFromIndex(i);
-            
-            // First we need to traverse the visual tree and find the stack panel which has the texxtblock inside of it.
-            // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
-            // on the Carousel itself. It is overly complicated for no reason.
-            var stackPanel = container.GetVisualDescendants().OfType<StackPanel>()
-                .FirstOrDefault(panel => panel.Name == "StackPanel");
-            var textBlock = stackPanel.GetVisualDescendants().OfType<TextBlock>()
-                .FirstOrDefault(textBlock => textBlock.Name == "DvData");
-            
-            // DV Data should only be displayed if the specific slide has measurments to be displayed.
-            var slide = (StepData)carousel.Items[i];
-            if (slide.Measurements != null)
+            // We need to loop through each Item to get each container for the slide
+            // to find the textblock.
+            for (var i = 0; i < carousel.ItemCount; i++)
             {
-                // Assert the textblock is visible.
-                Assert.IsTrue(textBlock.IsVisible);
-                Assert.That(textBlock.Text, Is.EqualTo("DV Data"));
-            }
-            else
-            {
-                // Assert it is not visible.
-                Assert.IsFalse(textBlock.IsVisible);
-            }
+                var container = carousel.ItemContainerGenerator.ContainerFromIndex(i);
 
-            // Change to next slide
-            carousel.Next();
-            // Update UI cause slide has changed.
-            Dispatcher.UIThread.RunJobs();
+                // First we need to traverse the visual tree and find the stack panel which has the texxtblock inside of it.
+                // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
+                // on the Carousel itself. It is overly complicated for no reason.
+                var stackPanel = container.GetVisualDescendants().OfType<StackPanel>()
+                    .FirstOrDefault(panel => panel.Name == "StackPanel");
+                var textBlock = stackPanel.GetVisualDescendants().OfType<TextBlock>()
+                    .FirstOrDefault(textBlock => textBlock.Name == "DvData");
+
+                // DV Data should only be displayed if the specific slide has measurments to be displayed.
+                var slide = (StepData)carousel.Items[i];
+                if (slide.Measurements != null)
+                {
+                    // Assert the textblock is visible.
+                    Assert.IsTrue(textBlock.IsVisible);
+                    Assert.That(textBlock.Text, Is.EqualTo("DV Data"));
+                }
+                else
+                {
+                    // Assert it is not visible.
+                    Assert.IsFalse(textBlock.IsVisible);
+                }
+
+                // Change to next slide
+                carousel.Next();
+                // Update UI cause slide has changed.
+                Dispatcher.UIThread.RunJobs();
+            }
         }
     }
 }
