04f674197261d58450a8aad17a79d4217e209aee
Erin Cate <77478941+erxncxte@users.noreply.github.com>
2024-09-22 21:33:32 +1000

    Fixed Status test, tidied up some spacing for Status page

--- DashboardTests/StatusPageTest.cs
+++ DashboardTests/StatusPageTest.cs
@@ -1,5 +1,8 @@
+using Avalonia.Controls;
 using Dashboard.Models;
 using Dashboard.ViewModels;
+using Avalonia.Headless.NUnit;
+using Dashboard.Views;
 using Moq;
 
 namespace DashboardTests;
@@ -16,68 +19,23 @@ public class OverviewPageTest
 
     private Mock<IDataStore> _dataStore;
 
-    [Test]
-    public void TestSpeedActual()
+    [AvaloniaTest]
+    public void TestThatStatusPageRendersCorrectly()
     {
-        _dataStore.SetupGet(x => x.SpeedActual).Returns(10);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.SpeedActual, Is.EqualTo(10));
-    }
+        var window = new StatusView
+        {
+            DataContext = new StatusViewModel(_dataStore.Object)
+        };
+        
+        Assert.IsNotNull(window);
 
-    [Test]
-    public void TestSpeedTarget()
-    {
-        _dataStore.SetupGet(x => x.SpeedTarget).Returns(10);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.SpeedTarget, Is.EqualTo(10));
-    }
+        // Make sure there is a icon for RES
+        var resIcon = window.FindControl<Image>("ResIcon");
+        Assert.IsNotNull(resIcon);
 
-    [Test]
-    public void TestSteeringAngleActual()
-    {
-        _dataStore.SetupGet(x => x.SteeringAngleActual).Returns(10);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.SteeringAngleActual, Is.EqualTo(10));
-    }
-
-    [Test]
-    public void TestSteeringAngleTarget()
-    {
-        _dataStore.SetupGet(x => x.SteeringAngleTarget).Returns(10);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.SteeringAngleTarget, Is.EqualTo(10));
-    }
-
-    [Test]
-    public void TestBrakePressureActual()
-    {
-        _dataStore.SetupGet(x => x.BrakePressureActual).Returns(10);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.BrakePressureActual, Is.EqualTo(10));
-    }
-
-    [Test]
-    public void TestBrakePressureTarget()
-    {
-        _dataStore.SetupGet(x => x.BrakePressureTarget).Returns(10);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.BrakePressureTarget, Is.EqualTo(10));
-    }
-
-    [Test]
-    public void TestRemoteEmergencyStopStatus()
-    {
-        _dataStore.SetupGet(x => x.RemoteEmergencyStopStatus).Returns(true);
-        var viewModel = new MainViewModel(_dataStore.Object);
-        Assert.That(viewModel.RemoteEmergencyStopStatus, Is.EqualTo(true));
-    }
-
-    [Test]
-    public void TestDispose()
-    {
-        var viewModel = new MainViewModel(_dataStore.Object);
-        viewModel.Dispose();
-        _dataStore.Verify(x => x.Dispose(), Times.Once);
+        Assert.Pass("Status page rendered correctly");
+        
     }
 }
 
+// More tests to be implemented when Data is connected to StatusView Page
