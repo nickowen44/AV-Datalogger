1b904ac438975a165ba385129a845c0ce0e52306
nrdag <25031285+nrdag@users.noreply.github.com>
2024-09-23 11:43:55 +1000

    Finalised about view

--- Dashboard/ViewModels/AboutViewModel.cs
+++ Dashboard/ViewModels/AboutViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Dashboard.ViewModels;
+﻿using System;
+
+namespace Dashboard.ViewModels;
 
 public class AboutViewModel : ViewModelBase
 {
     public static string GitCommit => ThisAssembly.Git.Commit;
     public static string GitBranch => ThisAssembly.Git.Branch;
+
+    public static string CurrentYear => DateTime.Now.Year.ToString();
 }
