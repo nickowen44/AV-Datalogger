6ea51e78486394f292698f68975c636cf061776f
AriadneVentura <s3902040@student.rmit.edu.au>
2024-09-24 14:39:28 +1000

    ADl-29 testing for reset feature

--- DashboardTests/ScrutineeringTests.cs
+++ DashboardTests/ScrutineeringTests.cs
@@ -70,43 +70,41 @@ public class ScrutineeringTests
         };
 
         window.Show();
-        // Get the Carousel
-        // Have to do it in this odd fashion as Avalonia does not trigger the full rendering process with a UserControl View
-        // and the Dv data was not being properly rendered so it was not being picked up.
-        foreach (var carousel in window.GetVisualDescendants().OfType<Carousel>())
-            // We need to loop through each Item to get each container for the slide
-            // to find the text block.
-            for (var i = 0; i < carousel.ItemCount; i++)
+        var carousel = window.GetVisualDescendants().OfType<Carousel>().FirstOrDefault();
+
+        // We need to loop through each Item to get each container for the slide
+        // to find the text block.
+        for (var i = 0; i < carousel.ItemCount; i++)
+        {
+            var container = carousel.ContainerFromIndex(i);
+
+            // First we need to traverse the visual tree and find the stack panel which has the text block inside of it.
+            // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
+            // on the Carousel itself. It is overly complicated for no reason.
+            var stackPanel = container?.GetVisualDescendants().OfType<StackPanel>()
+                .FirstOrDefault(panel => panel.Name == "StackPanel");
+            var textBlock = stackPanel?.GetVisualDescendants().OfType<TextBlock>()
+                .FirstOrDefault(textBlock => textBlock.Name == "DvData");
+
+            // DV Data should only be displayed if the specific slide has measurements to be displayed.
+            var slide = (StepData)carousel.Items[i]!;
+            if (slide.Measurements != null)
             {
-                var container = carousel.ContainerFromIndex(i);
-
-                // First we need to traverse the visual tree and find the stack panel which has the text block inside of it.
-                // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
-                // on the Carousel itself. It is overly complicated for no reason.
-                var stackPanel = container?.GetVisualDescendants().OfType<StackPanel>()
-                    .FirstOrDefault(panel => panel.Name == "StackPanel");
-                var textBlock = stackPanel?.GetVisualDescendants().OfType<TextBlock>()
-                    .FirstOrDefault(textBlock => textBlock.Name == "DvData");
-
-                // DV Data should only be displayed if the specific slide has measurements to be displayed.
-                var slide = (StepData)carousel.Items[i]!;
-                if (slide.Measurements != null)
-                {
-                    // Assert the text block is visible.
-                    Assert.IsTrue(textBlock?.IsVisible);
-                    Assert.That(textBlock?.Text, Is.EqualTo("DV Data"));
-                }
-                else
-                {
-                    // Assert it is not visible.
-                    Assert.IsFalse(textBlock?.IsVisible);
-                }
-
-                // Change to next slide
-                carousel.Next();
-                // Update UI cause slide has changed.
-                Dispatcher.UIThread.RunJobs();
+                // Assert the text block is visible.
+                Assert.IsTrue(textBlock?.IsVisible);
+                Assert.That(textBlock?.Text, Is.EqualTo("DV Data"));
             }
+            else
+            {
+                // Assert it is not visible.
+                Assert.IsFalse(textBlock?.IsVisible);
+            }
+
+            // Change to next slide
+            carousel.Next();
+            // Update UI cause slide has changed.
+            Dispatcher.UIThread.RunJobs();
+        }
     }
 
     [AvaloniaTest]
@@ -133,50 +131,48 @@ public class ScrutineeringTests
             .FirstOrDefault(e => e.Name == "AllStepsList");
         Assert.That(stepsList, Is.Not.Null);
 
-        // Get the Carousel
-        // Have to do it in this odd fashion as Avalonia does not trigger the full rendering process with a UserControl View
-        // and the Dv data was not being properly rendered so it was not being picked up.
-        foreach (var carousel in window.GetVisualDescendants().OfType<Carousel>())
-            // We need to loop through each Item to get each container for the slide
-            // to find the text block.
-            for (var i = 0; i < carousel.ItemCount; i++)
-            {
-                var container = carousel.ContainerFromIndex(i);
+        var carousel = window.GetVisualDescendants().OfType<Carousel>().FirstOrDefault();
 
-                // First we need to traverse the visual tree and find the stack panel which has the text block inside of it.
-                // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
-                // on the Carousel itself. It is overly complicated for no reason.
-                var stackPanel = container?.GetVisualDescendants().OfType<StackPanel>()
-                    .FirstOrDefault(panel => panel.Name == "StackPanel");
-                var passButton = stackPanel?.GetVisualDescendants().OfType<Button>()
-                    .FirstOrDefault(button => button.Content?.ToString() == "Pass");
-                var failButton = stackPanel?.GetVisualDescendants().OfType<Button>()
-                    .FirstOrDefault(button => button.Content?.ToString() == "Fail");
+        // We need to loop through each Item to get each container for the slide
+        // to find the text block.
+        for (var i = 0; i < carousel.ItemCount; i++)
+        {
+            var container = carousel.ContainerFromIndex(i);
 
-                Assert.That(passButton, Is.Not.Null);
-                Assert.That(failButton, Is.Not.Null);
+            // First we need to traverse the visual tree and find the stack panel which has the text block inside of it.
+            // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
+            // on the Carousel itself. It is overly complicated for no reason.
+            var stackPanel = container?.GetVisualDescendants().OfType<StackPanel>()
+                .FirstOrDefault(panel => panel.Name == "StackPanel");
+            var passButton = stackPanel?.GetVisualDescendants().OfType<Button>()
+                .FirstOrDefault(button => button.Content?.ToString() == "Pass");
+            var failButton = stackPanel?.GetVisualDescendants().OfType<Button>()
+                .FirstOrDefault(button => button.Content?.ToString() == "Fail");
 
-                // Test prior to button click that the step is "failed" by default
-                var items = stepsList.Items.OfType<TextBlock>().ToList();
-                Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Failed"));
+            Assert.That(passButton, Is.Not.Null);
+            Assert.That(failButton, Is.Not.Null);
 
-                // Click pass and update UI after button was clicked. Check expander shows passed.
-                passButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
-                Dispatcher.UIThread.RunJobs();
-                items = stepsList.Items.OfType<TextBlock>().ToList();
-                Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Passed"));
+            // Test prior to button click that the step is "failed" by default
+            var items = stepsList.Items.OfType<TextBlock>().ToList();
+            Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Failed"));
 
-                // Click fail and update UI after button was clicked. Check expander shows failed.
-                failButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
-                Dispatcher.UIThread.RunJobs();
-                items = stepsList.Items.OfType<TextBlock>().ToList();
-                Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Failed"));
+            // Click pass and update UI after button was clicked. Check expander shows passed.
+            passButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            Dispatcher.UIThread.RunJobs();
+            items = stepsList.Items.OfType<TextBlock>().ToList();
+            Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Passed"));
 
-                // Change to next slide
-                carousel.Next();
-                // Update UI cause slide has changed.
-                Dispatcher.UIThread.RunJobs();
-            }
+            // Click fail and update UI after button was clicked. Check expander shows failed.
+            failButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+            Dispatcher.UIThread.RunJobs();
+            items = stepsList.Items.OfType<TextBlock>().ToList();
+            Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Failed"));
+
+            // Change to next slide
+            carousel.Next();
+            // Update UI cause slide has changed.
+            Dispatcher.UIThread.RunJobs();
+        }
     }
 
     [AvaloniaTest]
@@ -203,74 +199,153 @@ public class ScrutineeringTests
         };
 
         window.Show();
-        // Get the Carousel
-        // Have to do it in this odd fashion as Avalonia does not trigger the full rendering process with a UserControl View
-        // and the Dv data was not being properly rendered so it was not being picked up.
-        foreach (var carousel in window.GetVisualDescendants().OfType<Carousel>())
-            // We need to loop through each Item to get each container for the slide
-            // to find the text block.
-            for (var i = 0; i < carousel.ItemCount; i++)
+
+        var carousel = window.GetVisualDescendants().OfType<Carousel>().FirstOrDefault();
+        // We need to loop through each Item to get each container for the slide
+        // to find the text block.
+        for (var i = 0; i < carousel.ItemCount; i++)
+        {
+            // First we need to traverse the visual tree and find the stack panel which has the text block inside of it.
+            // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
+            // on the Carousel itself. It is overly complicated for no reason.
+            var container = carousel.ContainerFromIndex(i);
+            var stackPanel = container?.GetVisualDescendants().OfType<StackPanel>()
+                .FirstOrDefault(panel => panel.Name == "StackPanel");
+
+            // Slides 7.2 and 7.5 and 7.8 together cover all 5 measurements that are to be checked in an Autonomous
+            // Vehicle Inspection.
+            if (i == 1)
             {
+                var contentControl = stackPanel?.GetVisualDescendants().OfType<ContentControl>()
+                    .FirstOrDefault(control => control.Name == "ContentControl");
+                var measurements = contentControl?.GetVisualDescendants().OfType<TextBlock>()
+                    .FirstOrDefault();
+
+                // 7.2 Contains Autonomous Mission Inspection
+                Assert.That(measurements?.Text, Is.EqualTo("Autonomous Mission Indicator: 2"));
+            }
+
+
+            if (i == 4)
+            {
+                var contentControl = stackPanel?.GetVisualDescendants().OfType<ContentControl>()
+                    .FirstOrDefault(control => control.Name == "ContentControl");
+                var textBlocks = contentControl?.GetVisualDescendants().OfType<TextBlock>().ToList();
+
+                // 7.5 Contains Autonomous System State and Steering Angle
+                var expectedTexts = new List<string>
+                {
+                    "Autonomous System State: 1",
+                    "Steering Angle: 90"
+                };
+
+                for (var j = 0; j < textBlocks?.Count; j++)
+                    Assert.That(textBlocks[j].Text, Is.EqualTo(expectedTexts[j]));
+            }
+
+
+            if (i == 7)
+            {
+                var contentControl = stackPanel?.GetVisualDescendants().OfType<ContentControl>()
+                    .FirstOrDefault(control => control.Name == "ContentControl");
+                var textBlocks = contentControl?.GetVisualDescendants().OfType<TextBlock>().ToList();
+
+                // 7.8 Contains Emergency Brake State, Service Brake State
+                var expectedTexts = new List<string>
+                {
+                    "Emergency Brake State: 3",
+                    "Service Brake State: true"
+                };
+
+                for (var j = 0; j < textBlocks?.Count; j++)
+                    Assert.That(textBlocks[j].Text, Is.EqualTo(expectedTexts[j]));
+            }
+
+            // Change to next slide
+            carousel.Next();
+            // Update UI cause slide has changed.
+            Dispatcher.UIThread.RunJobs();
+        }
+    }
+
+    [AvaloniaTest]
+    public void TestScrutineeringViewResetFeature()
+    {
+        // Arrange
+        // Create a window with the ScrutineeringView as its content for rendering purposes.
+        var window = new Window
+        {
+            Content = new ScrutineeringView(),
+            DataContext = new ScrutineeringViewModel(_dataStore.Object)
+        };
+
+        window.Show();
+
+        // Get the expander and the items inside it prior to checking each step
+        var expander = window.GetLogicalDescendants().OfType<Expander>()
+            .FirstOrDefault(e => e.Name == "AllStepsExpander");
+        Assert.That(expander, Is.Not.Null);
+
+        // It's not expanded by default so to test the contents we need to expand it.
+        expander.IsExpanded = true;
+        var stepsList = window.GetLogicalDescendants().OfType<ItemsControl>()
+            .FirstOrDefault(e => e.Name == "AllStepsList");
+        Assert.That(stepsList, Is.Not.Null);
+
+        var resetButton = window?.GetVisualDescendants().OfType<Button>()
+            .FirstOrDefault(button => button.Content?.ToString() == "Reset");
+        Assert.That(resetButton, Is.Not.Null);
+
+        var carousel = window.GetVisualDescendants().OfType<Carousel>().FirstOrDefault();
+        // We need to loop through each Item to get each container for the slide
+        // to find the text block.
+        for (var i = 0; i < carousel.ItemCount; i++)
+        {
+            // Pass a couple of steps.
+            if (i is 1 or 2 or 5)
+            {
+                var container = carousel.ContainerFromIndex(i);
+
                 // First we need to traverse the visual tree and find the stack panel which has the text block inside of it.
                 // We need to do this because items inside a DataTemplate is not directly accessible using FindControl
                 // on the Carousel itself. It is overly complicated for no reason.
-                var container = carousel.ContainerFromIndex(i);
                 var stackPanel = container?.GetVisualDescendants().OfType<StackPanel>()
                     .FirstOrDefault(panel => panel.Name == "StackPanel");
+                var passButton = stackPanel?.GetVisualDescendants().OfType<Button>()
+                    .FirstOrDefault(button => button.Content?.ToString() == "Pass");
 
-                // Slides 7.2 and 7.5 and 7.8 together cover all 5 measurements that are to be checked in an Autonomous
-                // Vehicle Inspection.
-                if (i == 1)
-                {
-                    var contentControl = stackPanel?.GetVisualDescendants().OfType<ContentControl>()
-                        .FirstOrDefault(control => control.Name == "ContentControl");
-                    var measurements = contentControl?.GetVisualDescendants().OfType<TextBlock>()
-                        .FirstOrDefault();
+                Assert.That(passButton, Is.Not.Null);
 
-                    // 7.2 Contains Autonomous Mission Inspection
-                    Assert.That(measurements?.Text, Is.EqualTo("Autonomous Mission Indicator: 2"));
-                }
-
-
-                if (i == 4)
-                {
-                    var contentControl = stackPanel?.GetVisualDescendants().OfType<ContentControl>()
-                        .FirstOrDefault(control => control.Name == "ContentControl");
-                    var textBlocks = contentControl?.GetVisualDescendants().OfType<TextBlock>().ToList();
-
-                    // 7.5 Contains Autonomous System State and Steering Angle
-                    var expectedTexts = new List<string>
-                    {
-                        "Autonomous System State: 1",
-                        "Steering Angle: 90"
-                    };
-
-                    for (var j = 0; j < textBlocks?.Count; j++)
-                        Assert.That(textBlocks[j].Text, Is.EqualTo(expectedTexts[j]));
-                }
-
-
-                if (i == 7)
-                {
-                    var contentControl = stackPanel?.GetVisualDescendants().OfType<ContentControl>()
-                        .FirstOrDefault(control => control.Name == "ContentControl");
-                    var textBlocks = contentControl?.GetVisualDescendants().OfType<TextBlock>().ToList();
-
-                    // 7.8 Contains Emergency Brake State, Service Brake State
-                    var expectedTexts = new List<string>
-                    {
-                        "Emergency Brake State: 3",
-                        "Service Brake State: true"
-                    };
-
-                    for (var j = 0; j < textBlocks?.Count; j++)
-                        Assert.That(textBlocks[j].Text, Is.EqualTo(expectedTexts[j]));
-                }
-
-                // Change to next slide
-                carousel.Next();
-                // Update UI cause slide has changed.
+                // Click pass and update UI after button was clicked. Check expander shows passed.
+                passButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
                 Dispatcher.UIThread.RunJobs();
+                var items = stepsList.Items.OfType<TextBlock>().ToList();
+                Assert.That(items[i].Text, Is.EqualTo($"Step 7.{i + 1} Passed"));
             }
+
+            // Change to next slide
+            carousel.Next();
+            // Update UI cause slide has changed.
+            Dispatcher.UIThread.RunJobs();
+        }
+
+        // Check we are at the last slide (13 slides but 12 cause 0 indexed)
+        var currentIndex = carousel.SelectedIndex;
+        Assert.That(currentIndex, Is.EqualTo(12));
+
+        // Click reset button, update UI that button was clicked
+        resetButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        Dispatcher.UIThread.RunJobs();
+
+        // Check the current slide is now the first slide
+        currentIndex = carousel.SelectedIndex;
+        Assert.That(currentIndex, Is.EqualTo(0));
+        
+        // Check all steps in the expander have been set to failed.
+        var expanderItems = stepsList.Items.OfType<TextBlock>().ToList();
+        for (var i = 0; i < carousel.ItemCount; i++)
+        {
+            Assert.That(expanderItems[i].Text, Is.EqualTo($"Step 7.{i + 1} Failed"));
+        }
     }
 }
