c4e268053a68ce0eb5830799ce207c29d1bde6d8
nrdag <25031285+nrdag@users.noreply.github.com>
2024-10-01 12:02:03 +1000

    Created a serilog sink for logging to our footer console

--- Dashboard/Utils/LoggingConfig.cs
+++ Dashboard/Utils/LoggingConfig.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Dashboard.Logging;
+using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
+using Serilog.Events;
 
 namespace Dashboard.Utils;
 
 public static class LoggingConfig
 {
+    public static LogEventSink LogEventSink { get; } = new();
+
     /// <summary>
     ///     Extends the <see cref="ILoggingBuilder" /> to add a configured Serilog logger.
     /// </summary>
@@ -22,10 +26,20 @@ public static class LoggingConfig
     /// <returns>A configured <see cref="Logger" />.</returns>
     private static Logger CreateLogger()
     {
+        var logLevel = LogEventLevel.Information;
+        // Set the log level to Debug if the DEBUG symbol is defined (during development).
+#if DEBUG
+        logLevel = LogEventLevel.Debug;
+#endif
+
         return new LoggerConfiguration()
             .MinimumLevel.Debug()
             .WriteTo.File("logs/dashboard.txt", rollingInterval: RollingInterval.Day)
-            .WriteTo.Console()
+            .WriteTo.Console(
+                logLevel,
+                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}"
+            )
+            .WriteTo.Sink(LogEventSink, LogEventLevel.Information)
             .CreateLogger();
     }
 }
